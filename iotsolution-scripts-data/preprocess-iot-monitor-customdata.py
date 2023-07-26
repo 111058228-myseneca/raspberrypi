c88d86ba84971440f71bfd63661f7871cb144a83
Srashidirad <srashidirad@myseneca.ca>
2023-07-26 16:45:39 -0400

    5 changes made a1 submission

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -202,7 +202,7 @@ latlong=lat:long'
      identifier = "IoT device performance and failures"
 
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='min,max,anomprob,trend,avg'
+     preprocesslogic='MIN,MAX,MEDIAN,VARIANCE,OUTLIERS,TREND'
      #preprocesslogic='dataage_-4_day,trend,min,max' # millisecond,second,minute,hour,day
      #preprocesslogic='dataage_-4_hour' # millisecond,second,minute,hour,day
 #     preprocesslogic='dataage_1_minute' # millisecond,second,minute,hour,day
